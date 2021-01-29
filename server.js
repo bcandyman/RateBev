3a645a9ea8bea089db5c035c48a085a544285f77
Benjamin Candy <bcandyman93@gmail.com>
2021-01-28 16:52:43 -0800

    Added connection to database and initial api routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
 const PORT = 3000;
 
-app.get('/', function(req, res) {
-  res.send('Hello World')
-})
+app.use('/api', require('./routes/api'));
 
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`)
+  console.log(`Listening on port ${PORT}`);
 });
