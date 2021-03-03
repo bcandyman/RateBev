87f7bc3661fa46524cf45966d6b237ac3cfd0d21
Benjamin Candy <bcandyman93@gmail.com>
2021-03-02 20:17:23 -0800

    Added routes to support new models.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,3 @@
-// const { mapFinderOptions } = require('sequelize/types/lib/utils');
 const router = require('express').Router();
 const db = require('../models');
 
@@ -8,28 +7,27 @@ router.route('/states')
     res.json(states);
   });
 
-// router.route('/state/:uuid')
-//   .get(async (req, res) => {
-//     const { uuid } = req.params;
-//     const state = await db.State.findOne({
-//       where: {
-//         uuid,
-//       },
-//     });
-//     res.json(state);
-//   });
-
 router.route('/cities')
   .get(async (req, res) => {
-    console.log('cities');
     const cities = await db.City.findAll();
     res.json(cities);
+  });
+
+router.route('/brewers')
+  .get(async (req, res) => {
+    const brewers = await db.Brewer.findAll();
+    res.json(brewers);
+  });
+
+router.route('/brews')
+  .get(async (req, res) => {
+    const brews = await db.Brew.findAll();
+    res.json(brews);
   });
 
 router.route('/city/:id')
   .get(async (req, res) => {
     const { id } = req.params;
-    console.log(id);
     const cities = await db.City.findOne({
       where: {
         id,
@@ -37,12 +35,16 @@ router.route('/city/:id')
       include: [db.State],
     });
     res.json(cities);
+  })
+  .post(async (req, res) => {
+    const { name, stateId } = req.body;
+    const cityOutput = await db.City.create({ name, stateId });
+    res.send(cityOutput).status(200);
   });
 
 router.route('/state/:id')
   .get(async (req, res) => {
     const { id } = req.params;
-    console.log(id);
     const state = await db.State.findOne({
       where: {
         id,
@@ -52,21 +54,86 @@ router.route('/state/:id')
     res.json(state);
   });
 
-router.route('/city')
+router.route('/brewer/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const brewer = await db.Brewer.findOne({
+      where: {
+        id,
+      },
+      include: [db.City],
+    });
+    res.json(brewer);
+  })
+  .post(async (req, res) => {
+    const { name, cityId } = req.body;
+    const brewerOutput = await db.Brewer.create({ name, cityId });
+    res.send(brewerOutput).status(200);
+  });
+
+router.route('/brew/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const brews = await db.Brew.findOne({
+      where: {
+        id,
+      },
+      include: [{
+        model: db.Brewer,
+        include: [{
+          model: db.City,
+          include: db.State,
+        }],
+      }, {
+        model: db.Style,
+        attributes: ['name'],
+      }],
+    });
+    res.json(brews);
+  })
   .post(async (req, res) => {
     const {
       name,
-      stateId,
+      brewerId,
+      styleId,
+      rating,
+      comment,
     } = req.body;
+    const brewOutput = await db.Brew.create({
+      name,
+      brewerId,
+      styleId,
+      rating,
+      comment,
+    });
+    res.send(brewOutput).status(200);
+  });
 
-    try {
-      const city = await db.City.create({
-        name, stateId,
-      });
-      res.json(city);
-    } catch (error) {
-      res.status(500).json(error);
-    }
+router.route('/style/:id')
+  .get(async (req, res) => {
+    const { id } = req.params;
+    const style = await db.Style.findOne({
+      where: {
+        id,
+      },
+    });
+    res.json(style);
+  })
+  .post(async (req, res) => {
+    const { name } = req.body;
+    const styleOutput = await db.Style.create({ name });
+    res.send(styleOutput).status(200);
+  });
+
+router.route('/brews/bystyle/:styleId')
+  .get(async (req, res) => {
+    const { styleId } = req.params;
+    const brewOutput = await db.Brew.findAll({
+      where: {
+        styleId,
+      },
+    });
+    res.json(brewOutput);
   });
 
 module.exports = router;
