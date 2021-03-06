56a5a8a0951193ef7934e7550b667f5224a76f51
Benjamin Candy <bcandy93@gmail.com>
2021-03-06 15:02:19 -0800

    Added express middleware to serve up client/build directory in production environment.

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
-const express = require('express');
-const { sequelize } = require('./models');
+require("dotenv").config();
+const express = require("express");
+const { sequelize } = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', require('./routes/api'));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+}
+
+app.use("/api", require("./routes/api"));
 
 app.listen(PORT, async () => {
-  console.log(`Listening on port ${PORT}`);
-  await sequelize.authenticate();
-  console.log('Database connected!');
+	console.log(`Listening on port ${PORT}`);
+	await sequelize.authenticate();
+	console.log("Database connected!");
 });
